8593c909b95537205b6c426565838818564b818e
Mayank1203 <mayankpatle1203@gmail.com>
2026-02-18 19:54:04 +0530

    FlatMap Function

---
+++ src/FlatMapExample.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class FlatMapExample {
+    public static void main(String[] args) {
+        List<List<String>> listofList = Arrays.asList(
+                Arrays.asList("m" , "a"),
+                Arrays.asList("y" , "a"),
+                Arrays.asList("n" , "k")
+        );
+
+        listofList.stream()
+                .flatMap(list->list.stream())
+                .map(c->c.toUpperCase())
+                .forEach(System.out::println);
+    }
+}
