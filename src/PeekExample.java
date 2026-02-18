d97e5f4603e0f109d24b3f516fdabccad2d0905c
Mayank1203 <mayankpatle1203@gmail.com>
2026-02-18 19:54:20 +0530

    Peek Function

---
+++ src/PeekExample.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PeekExample {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<Integer> integerList = list.stream()
+                .map(i -> i * i)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+    }
+}
