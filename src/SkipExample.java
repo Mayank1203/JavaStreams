6c72bcbebae532a093e00194f46fc87febdc4bb6
Mayank1203 <mayankpatle1203@gmail.com>
2026-02-18 19:54:58 +0530

    Skip Function

---
+++ src/SkipExample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SkipExample {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        list.stream().skip(2).forEach(System.out::println);
+    }
+}
