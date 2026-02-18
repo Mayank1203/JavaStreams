8c7e03fe7961bf961a460467475510de7947ff62
Mayank1203 <mayankpatle1203@gmail.com>
2026-02-18 19:54:44 +0530

    Reduce Function

---
+++ src/ReduceExample.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReduceExample {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4);
+
+        Integer reduce = list.stream()
+                .reduce(0, (a, b) -> a + b);
+
+        System.out.println(reduce);
+    }
+}
